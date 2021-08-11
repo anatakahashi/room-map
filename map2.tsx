8415923fafd61c98cbee917a5852fed785b0aaf9
anatakahashi <80654802+anatakahashi@users.noreply.github.com>
2021-08-11 03:41:01 -0300

    novo commit

---
+++ map2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="map2" tilewidth="32" tileheight="32" tilecount="1500" columns="50">
+ <image source="C:/Users/Ana Paula/Desktop/map2.png" width="1600" height="960"/>
+</tileset>
